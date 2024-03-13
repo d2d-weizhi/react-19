b1da10aa61fe208bf9ff2388890be78a3f9da4aa
shermannatrix <shermannatrix@gmail.com>
2024-03-13 11:42:09 +0800

    Using nodemon

--- reactjs-foundations/ch01-helloworld/src/App.js
+++ reactjs-foundations/ch01-helloworld/src/App.js
@@ -7,7 +7,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				Hello {personName}!
+				Hello {personName}, it is a great day to learn React 17!
 			</header>
 			<input type="text" onChange={(e) => setPersonName(e.target.value)} />
 		</div>
