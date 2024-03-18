9427aa8f25614de433e78d6d0d3bfa74f6022ac7
shermannatrix <shermannatrix@gmail.com>
2024-03-18 15:12:33 +0800

    The learrning experience has been really fruitful!

--- react-upandrunning/ch07-building-components/whinepad/src/components/Discovery.js
+++ react-upandrunning/ch07-building-components/whinepad/src/components/Discovery.js
@@ -1,14 +1,19 @@
 import Excel from './Excel';
-import React from 'react';
+import React, { useRef } from 'react';
 import Logo from './Logo';
 import Body from './Body';
 import Button from './Button';
 import './Discovery.css';
 import Suggest from './Suggest';
+import Rating from './Rating';
+import FormInput from './FormInput';
+import Form from './Form';
+import Actions from './Actions';
 
 const options = ['eenie', 'meenie', 'miney', 'mo'];
 
 function Discovery() {
+	const form = useRef();
 	return (
 		<div className="wrapper">
 			<h2>Logo</h2>
@@ -33,6 +38,76 @@ function Discovery() {
 			<p>
 				<Suggest options={options} />
 			</p>
+			<h2>Rating</h2>
+			<p>
+				No initial value: <Rating />
+			</p>
+			<p>
+				Initial value 4: <Rating defaultValue={4} />
+			</p>
+			<p>
+				This one goes to 11: <Rating max={11} />
+			</p>
+			<p>
+				Read-only: <Rating readonly={true} defaultValue={3} />
+			</p>
+			<h2>Form inputs</h2>
+			<table className="Discovery-pad">
+				<tbody>
+					<tr>
+						<td>Vanilla input</td>
+						<td><FormInput /></td>
+					</tr>
+					<tr>
+						<td>Prefilled</td>
+						<td><FormInput defaultValue="with a default" /></td>
+					</tr>
+					<tr>
+						<td>Year</td>
+						<td><FormInput type="year" /></td>
+					</tr>
+					<tr>
+						<td>Rating</td>
+						<td><FormInput type="rating" defaultValue={4} /></td>
+					</tr>
+					<tr>
+						<td>Suggest</td>
+						<td>
+							<FormInput
+								type="suggest"
+								options={['red', 'green', 'blue']}
+								defaultValue="green"
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>Vanilla textarea</td>
+						<td><FormInput type="textarea" /></td>
+					</tr>
+				</tbody>
+			</table>
+			<h2>Form Component</h2>
+			<Form
+				ref={form}
+				fields={{
+					rateme: { label: 'Rating', type: 'rating' },
+					freetext: { label: 'Greetings' },
+				}}
+				initialData={{ rateme: 4, freetext: 'Hello' }}
+			/>
+			<Button
+				onClick={() => {
+					const data = {};
+					Array.from(form.current).forEach(
+						(input) => (data[input.id] = input.value)
+					);
+					alert(JSON.stringify(data));
+				}}
+			>
+				Submit
+			</Button>
+			<h2>Actions Component</h2>
+			<Actions onAction={(type) => alert(type)} />
 			<h2>Excel</h2>
 			<Excel
 				headers={['Name', 'Year']}
--- react-upandrunning/ch07-building-components/whinepad/src/components/Rating.js
+++ react-upandrunning/ch07-building-components/whinepad/src/components/Rating.js
@@ -8,9 +8,30 @@ function Rating ({id, defaultValue = 0, max = 5, readonly = false}) {
 	const [tempRating, setTempRating] = useState(defaultValue);
 
 	const stars = [];
+	for (let i = 1; i <= max; i++) {
+		stars.push(
+			<span
+				className={i <= tempRating ? 'RatingOn' : null}
+				key={i}
+				onClick={() => (readonly ? null : setRating(i))}
+				onMouseOver={() => (readonly ? null : setTempRating(i))}
+			>
+				&#128514;
+			</span>
+		);
+	}
 
 	return (
-		<span>{stars}</span>
+		<span
+			className={classNames({
+				Rating: true,
+				RatingReadonly: readonly
+			})}
+			onMouseOut={() => setTempRating(rating)}
+		>
+			{stars}
+			<input id={id} type="hidden" value={rating} />
+		</span>
 	);
 }
 
