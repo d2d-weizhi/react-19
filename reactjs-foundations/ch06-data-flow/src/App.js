e159b740157a37fe09ded014e2ffadd79c218e7f
shermannatrix <shermannatrix@gmail.com>
2024-03-20 18:47:08 +0800

    I can move on to the next exercise. Niceee

--- reactjs-foundations/ch06-data-flow/src/App.js
+++ reactjs-foundations/ch06-data-flow/src/App.js
@@ -4,16 +4,20 @@
 // import MyComponent from './components/proptypes-func/MyComponent';
 // import SiteLink from './components/validating-nodes/SiteLink';
 // import BorderBox from './components/validating-elements/BorderBox';
-import Person from './components/validating-jsclass/Person';
-import FamilyTree from './components/validating-jsclass/FamilyTree';
+// import Person from './components/validating-jsclass/Person';
+// import FamilyTree from './components/validating-jsclass/FamilyTree';
+// import Contact from './components/custom-validator/Contact';
+import CounterClass from './components/pass-func-setstate/CounterClass';
 import './App.css';
 
 function App() {
   //const [theNumber, setTheNumber] = useState(0);
-  var clarence = new Person('Clarence');
+  // var clarence = new Person('Clarence');
   return (
     <div className="App">
-      <FamilyTree father={clarence} />
+      <CounterClass />
+      {/* <Contact fullName="Chen Weizhi" phone="555" /> */}
+      {/* <FamilyTree father={clarence} /> */}
       {/* <BorderBox>
         <p>The first paragraph</p>
         <p>The second paragraph</p>
---
+++ reactjs-foundations/ch06-data-flow/src/components/default-props/StoreList.jsx
@@ -0,0 +1,11 @@
+function StoreList (props) {
+	return (
+		<p>
+			{props.latitude}
+			<br />
+			{props.longitude}
+		</p>
+	);
+}
+
+export default StoreList;
