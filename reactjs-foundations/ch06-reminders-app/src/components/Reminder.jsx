cc5fbb8188ee4e61594dd6a03569e4a2f80c7223
shermannatrix <shermannatrix@gmail.com>
2024-03-21 17:01:53 +0800

    Worked on the reminders-app example using Functional Components.

--- reactjs-foundations/ch06-reminders-app/src/components/Reminder.jsx
+++ reactjs-foundations/ch06-reminders-app/src/components/Reminder.jsx
@@ -1,7 +1,34 @@
+import PropTypes from 'prop-types';
+
 function Reminder (props) {
+
+	function handleChange() {
+		props.setIsComplete(!props.isComplete, props.id);
+	}
 	return (
-		<div>Reminder</div>
+		<div className="item">
+			Item: {props.reminderText}{' '}
+			<span className="due-date">Due date: {props.dueDate}</span>
+			<span className="is-complete">
+				Completed?: <input type="checkbox" checked={props.isComplete} onChange={handleChange} />
+			</span>
+		</div>
 	);
+}
+
+Reminder.propTypes = {
+	reminderText: PropTypes.string,
+	dueDate: PropTypes.string,
+	isComplete: PropTypes.bool
+}
+
+const date = new Date();
+const formattedDate = date.toISOString().substring(0, 10);
+
+Reminder.defaultProps = {
+	reminderText: "No Reminder Set",
+	dueDate: formattedDate,
+	isComplete: false
 }
 
 export default Reminder;
