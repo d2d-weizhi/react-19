f0c70225051ef7ce2e67377fdbdeee03bea8a3da
shermannatrix <shermannatrix@gmail.com>
2024-03-21 19:26:03 +0800

    Time to save my work. I'm thinking of taking a break from this chapter to dive a little bit more into 'states and props'.

--- reactjs-foundations/ch07-events/src/App.js
+++ reactjs-foundations/ch07-events/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+// import ScreenDoor from './components/inline-handlers/ScreenDoor';
+// import Search from './components/function-component/Search';
+import CoffeeMachine from './components/class-component/CoffeeMachine';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <ScreenDoor /><br />
+			<Search /><br /> */}
+			<CoffeeMachine />
+		</div>
+	);
 }
 
 export default App;
