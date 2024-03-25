11f361198b16bdcee7fab5030f8f5239a70cfbc9
shermannatrix <shermannatrix@gmail.com>
2024-03-25 18:53:20 +0800

    Learned a few ways to use Ref.

---
+++ reactjs-foundations/ch09-refs/src/components/function-component/TextReader.jsx
@@ -0,0 +1,11 @@
+import { useRef } from 'react';
+
+function TextReader (props) {
+	const textView = useRef(null);
+
+	return (
+		<textarea ref={textView} value={props.bookText} />
+	);
+}
+
+export default TextReader;
