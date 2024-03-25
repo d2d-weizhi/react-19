e8780312cdae381ce6f79a810737652ce1a7f5fb
shermannatrix <shermannatrix@gmail.com>
2024-03-25 13:32:18 +0800

    Done with chapter 8.

--- reactjs-foundations/ch08-forms/src/App.js
+++ reactjs-foundations/ch08-forms/src/App.js
@@ -1,10 +1,14 @@
-import SignUp from './components/function-component/SignUp';
+// import SignUp from './components/class-component/SignUp';
+// import SearchBox from './components/lifting-up-state/SearchBox';
+import BlogComment from './components/uncontrolled-input/BlogComment';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
+      {/* <SearchBox /> */}
+      <BlogComment />
     </div>
   );
 }
---
+++ reactjs-foundations/ch08-forms/src/components/lifting-up-state/SearchResults.jsx
@@ -0,0 +1,7 @@
+function SearchResults (props) {
+	return (
+		<p>You're searching for: {props.searchTerm}</p>
+	);
+}
+
+export default SearchResults;
