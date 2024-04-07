08ee555c5f6bce09e0bd673d29d901c92c5b649b
Shermannatrix <shermannatrix@gmail.com>
2024-04-08 00:03:44 +0800

    Working through the React Routing chapter.
    I'm constantly trying to understand the difference between v5 and v6 of the react-router-dom usage.

---
+++ reactjs-foundations/ch12-routing/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import RouteRedirection from "./components/redirection/RouteRedirection";
+// import ComponentProp from "./components/render-props/ComponentProp";
+// import HomeScreen from './components/url-params/HomeScreen';
+// import ChooseYourAdventure from './components/reactrouterdom-v5/ChooseYourAdventure';
+// import MainMenu from './components/navlinks-exact/MainMenu';
+
+function App() {
+  return (
+    <div className="App">
+			<RouteRedirection />
+      {/* <ComponentProp /> */}
+      {/* <HomeScreen /> */}
+      {/* <MainMenu /> */}
+      {/* <ChooseYourAdventure /> */}
+    </div>
+  );
+}
+
+export default App;
