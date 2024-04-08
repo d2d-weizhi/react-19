c52a13c9ae5a0aa7f40e44e612c6617cbe869add
shermannatrix <shermannatrix@gmail.com>
2024-04-08 16:33:33 +0800

    I'm adapting from the online tutorial, structing the project in a way that I think makes sense (components-wise).

--- react-router-6/tutorial/react-router-contacts/src/App.jsx
+++ react-router-6/tutorial/react-router-contacts/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div id="sidebar">
+				<h1>React Router Contacts</h1>
+				<div>
+					<form id="search-form" role="search">
+						<input id="q"
+							aria-label="Search contacts"
+							placeholder="Search"
+							type="search"
+							name="q"
+						/>
+						<div
+							id="search-spinner"
+							aria-hidden
+							hidden={true}
+						/>
+						<div
+							className="sr-only"
+							aria-live="polite"
+						></div>
+					</form>
+					<form method="post">
+						<button type="submit">New</button>
+					</form>
+				</div>
+				<nav>
+					<ul>
+						<li><a href={`/contacts/1`}>Your Name</a></li>
+						<li><a href={`/contacts/2`}>You Friend</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div id="detail">
+				<Outlet />
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- react-router-6/tutorial/react-router-contacts/src/main.jsx
+++ react-router-6/tutorial/react-router-contacts/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+	createBrowserRouter,
+	RouterProvider
+} from 'react-router-dom';
+import App from './App.jsx';
+import ErrorPage from './components/ErrorPage';
+import Contact from './components/Contact';
 import './index.css'
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "contacts/:contactId",
+				element: <Contact />
+			}
+		]
+	},
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 )
