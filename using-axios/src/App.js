3b9a8898672da8475f3ecf543d2bd5cb14d4c899
Shermannatrix <shermannatrix@gmail.com>
2024-04-17 15:21:50 +0800

    Integration with the dropdownlist control from KendoReact is done.

--- using-axios/src/App.js
+++ using-axios/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import SkillsList from './components/axios-get-simple';
+// import SkillsList from './components/axios-get-simple';
+import IndustriesDDL from './components/axios-get-industries';
+import '@progress/kendo-theme-default/dist/all.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>List of Skills (by Keyword match)</h1>
-      <SkillsList />
+      <h1>List of Industries (KendoReact DropDownList)</h1>
+      {/* <SkillsList /> */}
+      <IndustriesDDL />
     </div>
   );
 }
