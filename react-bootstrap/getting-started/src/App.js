672a4b59b51fd2ece487317fdd6c5ee01d4c3494
Shermannatrix <shermannatrix@gmail.com>
2024-05-07 00:23:52 +0800

    A renewed effort to dive into all-things react-bootstrap

---
+++ react-bootstrap/getting-started/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import AlertDismissible from './with-state';
+
+function App() {
+  return (
+    <div className="App">
+      <AlertDismissible />
+    </div>
+  );
+}
+
+export default App;
