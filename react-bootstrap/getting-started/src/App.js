af3adc311a0e976f58ec6341060f5f6a2c3a632f
shermannatrix <shermannatrix@gmail.com>
2024-05-07 12:55:27 +0800

    getting start section was nice.

--- react-bootstrap/getting-started/src/App.js
+++ react-bootstrap/getting-started/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import AlertDismissible from './with-state';
+import ColorModesDemo from './color-modes';
+// import AlertDismissible from './with-state';
 
 function App() {
   return (
     <div className="App">
-      <AlertDismissible />
+      <ColorModesDemo />
+      {/* <AlertDismissible /> */}
     </div>
   );
 }
